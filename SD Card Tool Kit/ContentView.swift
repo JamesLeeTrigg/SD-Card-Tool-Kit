fa2bb4955a37f0a5a94c03f6ce7bef2e640c29e1
James Trigg <james@jamestrigg.co.uk>
2020-09-12 16:23:35 +0100

    Initial Commit

---
+++ SD Card Tool Kit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SD Card Tool Kit
+//
+//  Created by James Trigg on 12/09/2020.
+//  Copyright © 2020 James Trigg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
